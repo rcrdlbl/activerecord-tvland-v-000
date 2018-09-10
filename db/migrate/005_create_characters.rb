4c47bc0e0d4c91884d7ae917282ed9b086c8fb39
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-10 02:18:06 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,9 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
-    
+    create_table :characters do |t|
+      t.string :name
+      t.actor_id :integer
+      t.show_id :integer
+    end
   end
 end
