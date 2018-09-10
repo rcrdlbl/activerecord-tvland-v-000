8fe33e4cd46cd6e5cd00d3fa1517ca6b65eafc67
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-10 02:35:32 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
-      t.show_id :integer
+      t.integer :show_id
     end
   end
 end
